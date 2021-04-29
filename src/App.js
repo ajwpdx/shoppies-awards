d8527d5a49e9acf0e1e477f52566bd50942316c9
Alex Williams <alexjwilliams1@gmail.com>
2021-04-29 16:37:32 -0700

    access to api using axios with private key

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 //components
 import Nominations from './layouts/Nominations';
@@ -6,6 +6,8 @@ import SearchResults from './layouts/SearchResults';
 import TitleHeader from './layouts/TitleHeader';
 
 function App() {
+  const [nominations, setNominations] = useState([])
+
   return (
     <div className="App">
       <TitleHeader/>
--- src/layouts/SearchResults.js
+++ src/layouts/SearchResults.js
@@ -1,20 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import axios from "axios"
 
 
 const SearchResults = (props) => {
+    const [searchText, setSearchText] = useState("")
+    const [searchList, setSearchList] = useState([])
 
-    const [nominations, setNominations] = useState([])
-    const []
+    const onSearchTextChange = (evt) =>{
+        setSearchText(evt.target.value)
+    }
+
+    // useEffect(()=>{
+
+    // }, [searchText])
+
+    const onSearchSubmit = (evt) => {
+        evt.preventDefault()
+        axios.get(`http://www.omdbapi.com/?apikey=66b9867&s=${searchText}`)
+        .then(res=> console.log(res.data))
+        .catch(err => console.log("error ->", err))
+        
+
+    }
 
     return (
         <section id="search">
             <h2 className='section-title'>Search Movies</h2>
             <div className='search-area'>
-                <form className='search-form'>
+                <form className='search-form' onSubmit={onSearchSubmit}>
                     <input
                         placeholder='Movie Title'
+                        type="search" 
+                        results="0"
+                        value={searchText}
+                        onChange={onSearchTextChange}
                     />
+                    <button >Search</button>
                 </form>
                 <div className='movies-list'>
 
